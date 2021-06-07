73042cef37375a5dd583fedac37a675614a2f8cf
Keerthi Vasan GC <33746143+armstrong153@users.noreply.github.com>
2021-06-07 02:43:43 -0700

    working light version
    the note placement still needs work. Currently it just places it at the end of the parentNode.

--- src/editor-light.js
+++ src/editor-light.js
@@ -1,6 +1,3 @@
-/// editor.js but with a less fancier note editor but this implemtnation does not require any external libraries
-
-
 //// variables
 var note_count=1;
 
@@ -24,45 +21,43 @@ function highlightText(e){
 				newNode.setAttribute("style", "background-color:#d9ffcc;");
 				newNode.addEventListener('mousedown', removeHighlight); 
 				function removeHighlight(){	
-					newNode.setAttribute("style", "");  //removes the highlight
+				newNode.setAttribute("style", "");  //removes the highlight
 				}
 
-
-				newNode.appendChild(range.extractContents());``
+				
+				newNode.appendChild(range.extractContents());
 				range.insertNode(newNode);
         toggleHighlight = true;
 
-      }
-
-    
-    }
         
-
-        
-              
-        
-  if(e.keyCode ==49){	
 			////////// annotate ///////////
-			if(window.getSelection().rangeCount >0){
-				var newNode1 = document.createElement("div");
-				document.body.appendChild(newNode1);
-				newNode1.classList.add("ui-widget-content");
-        document.body.appendChild(newNode1)
 
+				var newNode1 = document.createElement("div");
+        newNode1.offsetLeft=newNode.offsetLeft;
+        newNode1.offsetTop=newNode.offsetTop+20;     
         
-				newNode1.setAttribute("style", "z-index:100;display: inline-block;height: 375px; resize: both; overflow:auto;");    
+				newNode1.setAttribute("style", "z-index:100;display: inline-block;height: 100px; resize: both; overflow:auto; position: relative; left: 20px;");    
+         newNode.parentNode.appendChild(newNode1);
+
+//         range.insertNode(newNode1);
 
 
 			//   very simple sticky note interface 
 			   newNode1.innerHTML= `<p contenteditable="true" style="background-color: #ffffcc;border: none;color: black;  padding: 15px 32px; text-align: enter;text-decoration: none;  display: inline-block;  font-size: 16px; resize: both; overflow:auto;" >`+ "Annotate here" +"</p>";
+        
+        newNode1.addEventListener('dblclick', removeAnnotate); 
+				function removeAnnotate(){	
+//           newNode1.innertHTML="";              //removes the note
+// 					newNode1.setAttribute("style", "");  //removes the highlight
+          newNode1.remove();
+				}
+        
 
 		note_count+=1; // update note counter 
 
 }   
+
+
 }
+
 }
-
-
-
-
-
